9cbcf6b0faba218d3021498008fcdd4b81249f84
Modather Ali <modather0ali@gmail.com>
2024-01-12 16:09:54 +0200

    new updates

--- lib/services/api/chat_services.dart
+++ lib/services/api/chat_services.dart
@@ -10,31 +10,13 @@ class ChatServices {
   static String freeGPTUrl = dotenv.env['FREE_GPT_URL']!;
 
   static Stream<Response>? receiveMessageFromAI(String prompt) {
-    // String responseMessage;
     Stream<Response>? stream;
     try {
       final response = http.post(
         Uri.parse(freeGPTUrl),
         body: {'message': prompt},
       );
-      // response.asStream().listen((event) {
-      //   Logger.print(event.body);
-      // });
       stream = response.asStream();
-      // return stream;
-      // StreamSubscription streamSubscription =
-      //     response.asStream().listen((event) {
-      //   Logger.print(event.body);
-      //   // responseMessage = event.body;
-      // });
-      // streamSubscription.
-      // if (response.statusCode.toString().startsWith('20')) {
-      //   final data = jsonDecode(response.body);
-      //   responseMessage = '$data';
-      //   log('$data');
-      // } else {
-      //   throw Exception('Failed to generate text: ${response.statusCode}');
-      // }
     } catch (e) {
       stream = Stream.error('Error');
       Logger.print('ai cant response: $e');
